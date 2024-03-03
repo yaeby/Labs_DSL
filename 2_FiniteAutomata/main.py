4ebf11901ce806e3b79338c8e81774c3a41d9308
yaeby <adrian.copta@isa.utm.md>
2024-03-03 22:44:14 +0200

    print

--- 2_FiniteAutomata/main.py
+++ 2_FiniteAutomata/main.py
@@ -15,15 +15,15 @@ transitions = {
 initial_state = 'q0'
 final_states = {'q4'}
 
-# states = {'q0', 'q1', 'q2'}
-# input_symbols = {'0', '1'}
-# transitions = {
-#     'q0': {'0': 'q0', '1': 'q1'},
-#     'q1': {'0': 'q0', '1': 'q2'},
-#     'q2': {'0': 'q2', '1': 'q1'},
-# }
-# initial_state = 'q0'
-# final_states = {'q1'}
+states = {'q0', 'q1', 'q2'}
+input_symbols = {'0', '1'}
+transitions = {
+    'q0': {'0': 'q0', '1': 'q1'},
+    'q1': {'0': 'q0', '1': 'q2'},
+    'q2': {'0': 'q2', '1': 'q1'},
+}
+initial_state = 'q0'
+final_states = {'q1'}
 
 
 fa = FiniteAutomata(states, input_symbols, transitions, initial_state, final_states)
@@ -32,11 +32,11 @@ result = fa.is_deterministic()
 if result:
     print("The Finite Automaton is deterministic.")
     regular_grammar = fa.dfa_to_regular_grammar()
-    print(regular_grammar)
+    Print.print_grammar(Vn=states, Vt=input_symbols, regular_grammar=regular_grammar)
 else:
     print("The Finite Automaton is not deterministic.")
     regular_grammar = fa.nfa_to_regular_grammar()
-    Print.print_grammar(regular_grammar=regular_grammar)
+    Print.print_grammar(Vn=states, Vt=input_symbols, regular_grammar=regular_grammar)
 
 # production_strings = []
 # # Print the resulting regular grammar with '|' symbol
